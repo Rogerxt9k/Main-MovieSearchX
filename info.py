c117f1dc88c1b30008376765bb1ef4abc127162c
RazerXT <101035245+RazerXT@users.noreply.github.com>
2024-03-01 15:23:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-import re
+Timport re
 from os import environ,getenv
 from Script import script 
 
@@ -51,7 +51,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'vipurl.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '1190882c4c34d0b817590bfe8fc5cc1715e2dccc')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
