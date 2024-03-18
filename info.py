d5e40f061f265a872b6597ec5f16cb54dc7cefff
RazerXT <101035245+RazerXT@users.noreply.github.com>
2024-03-18 11:19:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'earnpro.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '1ab8c8c9b9e12a952ea920b3814768e45b975449')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
